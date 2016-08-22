17a177c0f693b5a2a7dc3f303eb6002125434302
Felipe <notclevermail@gmail.com>
2016-08-21 19:31:52 -0500

    Felipe: esqueleto componente de configuracion de bicicletas - partes

---
+++ configurador-bici-rest/src/main/java/co/rcbike/configurador_bici/model/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Entidades (objetos de dominio) usadas para la configuracion de bicicletas
+ *
+ * @since 1.0
+ * @author jwhite
+ * @version 1.1
+ */
+package co.rcbike.configurador_bici.model;
