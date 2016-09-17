60cc02a44198e180a9e55b54a79cedc854082a9d
Felipe <notclevermail@gmail.com>
2016-09-16 22:12:06 -0500

    sql de usuario nuevo

--- usuarios-rest/src/main/resources/import.sql
+++ usuarios-rest/src/main/resources/import.sql
@@ -1,2 +1,3 @@
 -- You can use this file to load seed data into the database using SQL statements
 insert into Usuario (id, email, nombres, apellidos) values (0, 'john.smith@mailinator.com', 'John', 'Smith');
+insert into Usuario (id, email, nombres, apellidos) values (1, 'rcbike1@mail.com', 'rcbike1', 'rcbike1');
