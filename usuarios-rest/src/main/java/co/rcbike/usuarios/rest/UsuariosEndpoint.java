b2818732d53fd3bb59e87b8281bf2271909f7c6a
Felipe <notclevermail@gmail.com>
2016-10-09 17:31:50 -0500

    persistir amigos

--- usuarios-rest/src/main/java/co/rcbike/usuarios/rest/UsuariosEndpoint.java
+++ usuarios-rest/src/main/java/co/rcbike/usuarios/rest/UsuariosEndpoint.java
@@ -10,6 +10,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -41,12 +42,20 @@ public class UsuariosEndpoint {
         }
         return member;
     }
+
     @GET
     @Path("/amigos/{email: .+@.+}")
     @Produces(MediaType.APPLICATION_JSON)
     public List<Usuario> listAmigos(@PathParam("email") String email) {
         lookupUsuarioByEmail(email);
         return service.listAmigos(email);
+    }
+
+    @GET
+    @Path("/noamigos/{email: .+@.+}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Usuario> listNoAmigos(@PathParam("email") String email, @QueryParam("filtro") String filtro) {
+        return service.listNoAmigosDe(email, filtro);
     }
 
     @POST
@@ -58,6 +67,21 @@ public class UsuariosEndpoint {
             // TODO manejo correcto de excepcion
             e.printStackTrace();
         }
+        return Response.ok().build();
+    }
+
+    @POST
+    @Path("agregar-amigo")
+    @Consumes({MediaType.APPLICATION_JSON})
+    public Response agregarAmigo(List<String> emails) {
+        service.agregarAmigo(emails.get(0), emails.get(1));
+        return Response.ok().build();
+    }
+
+    @POST
+    @Path("remover-amigo")
+    public Response removerAmigo(List<String> emails) {
+        service.removerAmigo(emails.get(0), emails.get(1));
         return Response.ok().build();
     }
 
--- usuarios-rest/src/main/java/co/rcbike/usuarios/service/UsuariosService.java
+++ usuarios-rest/src/main/java/co/rcbike/usuarios/service/UsuariosService.java
@@ -1,13 +1,17 @@
 package co.rcbike.usuarios.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.TypedQuery;
+
+import com.google.common.base.Strings;
 
 import co.rcbike.autenticacion.service.AutenticacionService;
 import co.rcbike.usuarios.model.RegistroUsuario;
@@ -64,17 +68,44 @@ public class UsuariosService {
 
     }
 
-    public void agregarAmigo(String emailUsuario, String emailAmigo) {
+    public void removerAmigo(String emailUsuario, String emailAmigo) {
+        Usuario usuario = findUsuario(emailUsuario);
+        Usuario amigo = findUsuario(emailAmigo);
 
+        usuario.getAmigos().remove(amigo);
+        em.merge(amigo);
     }
 
-    public void removerAmigo(String emailUsuario, String emailAmigo) {
+    public void agregarAmigo(String emailUsuario, String emailAmigo) {
+        Usuario usuario = findUsuario(emailUsuario);
+        Usuario amigo = findUsuario(emailAmigo);
 
+        usuario.getAmigos().add(amigo);
+        em.merge(amigo);
     }
 
     public List<Usuario> listAmigos(String email) {
         List<Usuario> amigos = findUsuario(email).getAmigos();
         amigos.size();
-        return amigos;
+        return new ArrayList<>(amigos);
+    }
+
+    public List<Usuario> listNoAmigosDe(String email, String filtro) {
+        Usuario usuario = findUsuario(email);
+
+        List<String> emailExcluidos = listAmigos(email).stream().map(item -> item.getEmail())
+                .collect(Collectors.toList());
+
+        emailExcluidos.add(usuario.getEmail());
+
+        TypedQuery<Usuario> q = null;
+        if (!Strings.isNullOrEmpty(filtro)) {
+            q = em.createNamedQuery(Usuario.SQ_listNoAmigosFiltro, Usuario.class);
+            q.setParameter(Usuario.SQ_PARAM_FILTRO, "%" + filtro.toLowerCase() + "%");
+        } else {
+            q = em.createNamedQuery(Usuario.SQ_listNoAmigos, Usuario.class);
+        }
+        q.setParameter(Usuario.SQ_PARAM_EXCLUIDOS, emailExcluidos);
+        return q.getResultList();
     }
 }
