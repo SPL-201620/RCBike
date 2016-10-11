4de5019a0a9ebae9a345143556cb27aa34a1a7f7
juanc <juanc@juanc>
2016-10-10 22:59:05 -0500

    Back avance

--- mensajeria-rest/src/main/java/co/rcbike/mensajeria/service/MensajeriaService.java
+++ mensajeria-rest/src/main/java/co/rcbike/mensajeria/service/MensajeriaService.java
@@ -1,44 +1,91 @@
 package co.rcbike.mensajeria.service;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
 import co.rcbike.mensajeria.model.Conversacion;
 import co.rcbike.mensajeria.model.Mensaje;
-import javax.persistence.TypedQuery;
-import org.slf4j.Logger;
 
 @Stateless
 public class MensajeriaService {
 
-    @Inject
-    private Logger LOG;
+	@Inject
+	private EntityManager em;
+	private List<Mensaje> mensajes;
+	private Mensaje nuevoMensaje;
+	private String email;
+	private Conversacion nuevaConversacion;
 
-    @Inject
-    private EntityManager em;
-    private List<Mensaje> mensajes;
+	public List<Mensaje> findConvesacionByEmRes(String emailEmisor,
+			String emailReceptor) {
+		TypedQuery<Conversacion> q = em
+						.createNamedQuery(Conversacion.SQ_LISTBYEMAILS, Conversacion.class);		
+		q.setParameter("emailEmisor", emailEmisor);
+		q.setParameter("emailReceptor", emailReceptor);
+		try {
+			Conversacion c =q.getSingleResult();
+			return mensajes;
+		} catch (Exception e) {
+			return Collections.EMPTY_LIST;
+		}
 
-    public List<Mensaje> findConvesacionByEmRes(String emailEmisor, String emailReceptor) {
-        javax.persistence.Query q = em.createNamedQuery(Conversacion.SQ_LISTBYEMAILS);
-        q.setParameter("emailEmisor", emailEmisor);
-        q.setParameter("emailReceptor", emailReceptor);
-        Conversacion c = (Conversacion) q.getSingleResult();
-        if (c != null){
-           mensajes=c.getMensajes();
-        }
-        return mensajes;
-    }
+	}
 
-    public Conversacion create(Conversacion record) {
-        em.persist(record);
-        em.flush();
-        return record;
-    }
-    
-    public Mensaje create(Mensaje record) {
-        em.persist(record);
-        em.flush();
-        return record;
-    }
+	public Conversacion createConversacion(Mensaje mensaje, String emailReceptor) {
+		nuevaConversacion.setEmailReceptor(emailReceptor);
+		nuevaConversacion.setEmailEmisor(mensaje.getEmailEmisorMensaje());
+		em.persist(nuevaConversacion);
+		em.flush();
+		return nuevaConversacion;
+	}
+
+	public Mensaje createMensaje(Mensaje mensaje, String emailReceptor) {
+		nuevoMensaje = mensaje;
+		email = emailReceptor;
+		if (mensaje.getConversacion() != null) {
+			em.persist(mensaje);
+			em.flush();
+		} else {
+			createConversacion(nuevoMensaje, email);
+		}
+		return mensaje;
+	}
+
+	public List<Mensaje> getMensajes() {
+		return mensajes;
+	}
+
+	public void setMensajes(List<Mensaje> mensajes) {
+		this.mensajes = mensajes;
+	}
+
+	public Mensaje getNuevoMensaje() {
+		return nuevoMensaje;
+	}
+
+	public void setNuevoMensaje(Mensaje nuevoMensaje) {
+		this.nuevoMensaje = nuevoMensaje;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Conversacion getNuevaConversacion() {
+		return nuevaConversacion;
+	}
+
+	public void setNuevaConversacion(Conversacion nuevaConversacion) {
+		this.nuevaConversacion = nuevaConversacion;
+	}
 }
