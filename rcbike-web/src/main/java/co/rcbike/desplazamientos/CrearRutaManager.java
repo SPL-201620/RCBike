7924fd76de7c7648622aefa768012c6002701c36
Felipe <notclevermail@gmail.com>
2016-10-11 00:23:02 -0500

    creacion de desplazamientos

---
+++ rcbike-web/src/main/java/co/rcbike/desplazamientos/CrearRutaManager.java
@@ -0,0 +1,13 @@
+package co.rcbike.desplazamientos;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@SuppressWarnings("serial")
+@ManagedBean
+@ViewScoped
+public class CrearRutaManager implements Serializable {
+
+}
