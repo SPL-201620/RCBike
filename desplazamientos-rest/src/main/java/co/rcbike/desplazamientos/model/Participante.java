4abcc6846e578b1fddf2382be8265bcf2a862f71
Armando Perea Mora <a.perea10@uniandes.edu.co>
2016-10-24 19:39:03 -0500

    Completando metodos Backend desplazamientos y pruebas

--- desplazamientos-rest/src/main/java/co/rcbike/desplazamientos/model/Participante.java
+++ desplazamientos-rest/src/main/java/co/rcbike/desplazamientos/model/Participante.java
@@ -3,11 +3,10 @@ package co.rcbike.desplazamientos.model;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.validation.constraints.NotNull;
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -17,17 +16,20 @@ import org.hibernate.validator.constraints.NotEmpty;
 @SuppressWarnings("serial")
 @Entity
 @XmlRootElement
+@NamedQueries({ @NamedQuery(name = "findAllParticipantes", query = "SELECT p FROM Participante p"),
+	@NamedQuery(name = "findParticipantesByIdRuta", query = "SELECT p FROM Participante p WHERE p.idRuta = :idRuta") })
 public class Participante implements Serializable {
 
-	/*No es necesario llenar*/
+	public static final String SQ_findAllParticipantes = "findAllParticipantes";
+	public static final String SQ_findParticipantesByIdRuta = "findParticipantesByIdRuta";
+	public static final String SQ_PARAM_ID_RUTA = "idRuta";
+
 	@Id
 	@GeneratedValue
 	private Long id;
 
-	/*No es necesario llenar*/
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "ID_RUTA")
-	private Ruta ruta;
+	@NotNull
+	private Long idRuta;
 
 	@NotNull
 	@NotEmpty
@@ -42,14 +44,13 @@ public class Participante implements Serializable {
 		this.id = id;
 	}
 
-	public Ruta getRuta() {
-		return ruta;
+	public Long getIdRuta() {
+		return idRuta;
 	}
 
-	public void setRuta(Ruta ruta) {
-		this.ruta = ruta;
+	public void setRuta(Long idRuta) {
+		this.idRuta = idRuta;
 	}
-
 	public String getEmail() {
 		return email;
 	}
--- desplazamientos-rest/src/main/resources/import.sql
+++ desplazamientos-rest/src/main/resources/import.sql
@@ -1,8 +1,22 @@
+
 -- You can use this file to load seed data into the database using SQL statements
 -- Rutas
-insert into Ruta (id,emailCreador,tipo,latitudInicio,longitudInicio,latitudFinal,longitudFinal,distancia,tiempoEstimado,calorias,clima,fecha,frecuente) values(0,'rcb1@m.co','INDIVIDUAL','4.679566','-74.038578','4.673995','-74.042376',100.0,29,33,'21ºC',{ts '2017-09-17 18:47:52.69'},false);
-insert into Ruta (id,emailCreador,tipo,latitudInicio,longitudInicio,latitudFinal,longitudFinal,distancia,tiempoEstimado,calorias,clima,fecha,frecuente) values(1,'rcb2@m.co','GRUPAL','4.679566','-74.038578','4.673995','-74.042376',100.0,29,33,'16ºC',{ts '2017-09-17 18:47:52.69'},false);
+--insert into Ruta (id,emailCreador,tipo,latitudInicio,longitudInicio,latitudFinal,longitudFinal,distancia,tiempoEstimado,calorias,clima,fecha,frecuente) values(0,'rcb1@m.co','INDIVIDUAL','4.679566','-74.038578','4.673995','-74.042376',100.0,29,33,'21ºC',{ts '2017-09-17 18:47:52.69'},false);
+--insert into Ruta (id,emailCreador,tipo,latitudInicio,longitudInicio,latitudFinal,longitudFinal,distancia,tiempoEstimado,calorias,clima,fecha,frecuente) values(1,'rcb2@m.co','GRUPAL','4.679566','-74.038578','4.673995','-74.042376',100.0,29,33,'16ºC',{ts '2017-09-17 18:47:52.69'},false);
 
+insert into Ruta (id,nombre,descripcion,emailCreador,tipo,latitudInicio,longitudInicio,latitudFinal,longitudFinal,distancia,tiempoEstimado,calorias,clima,fecha,frecuente) values(-1,'Ruta 1', 'Ruta 1','maria.a@email.com','INDIVIDUAL',4.614499,-74.091609,4.633060,-74.070538,100.0,29,33,'16ºC',{ts '2016-10-24 18:47:52.69'},'N');
+insert into Ruta (id,nombre,descripcion,emailCreador,tipo,latitudInicio,longitudInicio,latitudFinal,longitudFinal,distancia,tiempoEstimado,calorias,clima,fecha,frecuente) values(-2,'Ruta 2', 'Ruta 2','alfon.cardozo@mensajeria.com','GRUPAL',4.603195,-74.065271,4.594787,-74.077964,100.0,29,33,'17ºC',{ts '2016-10-25 18:47:52.69'},'S');
 
 -- WayPoints
--- insert into WaypointRuta (id, idRuta, latitudInicio, longitudInicio) values (0, 1, 3.5, 4.2);
+insert into Waypoint (id,idRuta,latitud,longitud) values (-1,-1,4.610689,-74.090426);
+insert into Waypoint (id,idRuta,latitud,longitud) values (-2,-1,4.619574,-74.087576);
+insert into Waypoint (id,idRuta,latitud,longitud) values (-3,-1,4.624982,-74.083328);
+insert into Waypoint (id,idRuta,latitud,longitud) values (-4,-1,4.628634,-74.080409);
+insert into Waypoint (id,idRuta,latitud,longitud) values (-5,-1,4.632171,-74.079961);
+insert into Waypoint (id,idRuta,latitud,longitud) values (-6,-1,4.634213,-74.079774);
+insert into Waypoint (id,idRuta,latitud,longitud) values (-7,-1,4.634312,-74.077397);
+
+--Participantes
+insert into Participante (id, idRuta, email) values (-1, -2, 'ja.velandia@correo.com');
+insert into Participante (id, idRuta, email) values (-2, -2, 'maria.a@email.com');
+insert into Participante (id, idRuta, email) values (-3, -2, 'miguel.espitia@messages.co');
