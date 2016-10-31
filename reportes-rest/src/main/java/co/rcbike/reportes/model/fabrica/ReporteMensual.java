61e20fc2593733a2787dd81c73f173744c053e77
Armando Perea Mora <a.perea10@uniandes.edu.co>
2016-10-30 23:13:40 -0500

    Implementación Resumenes Mensuales y semanales

--- reportes-rest/src/main/java/co/rcbike/reportes/model/fabrica/ReporteMensual.java
+++ reportes-rest/src/main/java/co/rcbike/reportes/model/fabrica/ReporteMensual.java
@@ -10,14 +10,22 @@ public class ReporteMensual extends ReporteBasico implements IReporteRecorridos 
 
 	@Override
 	protected ResumenWeb crearResumen(Date fecha) {
-		GregorianCalendar gc = new GregorianCalendar();
-		gc.setTime(fecha);
-		GregorianCalendar fechaInicio = new GregorianCalendar(gc.get(Calendar.YEAR), gc.get(Calendar.MONTH), 1);
-		GregorianCalendar fechaFinal = new GregorianCalendar(gc.get(Calendar.YEAR), gc.get(Calendar.MONTH), 1);
-		fechaFinal.add(Calendar.DAY_OF_MONTH, -1);
 		ResumenWeb result = new ResumenWeb();
-		result.setFechaInicio(fechaInicio.getTime());
-		result.setFechaFinal(fechaFinal.getTime());
+
+		GregorianCalendar cal = new GregorianCalendar();
+		cal.setTime(fecha);
+
+		GregorianCalendar gc = new GregorianCalendar(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), 1);
+		gc.set(Calendar.HOUR_OF_DAY, 0); // ! clear would not reset the hour of day !
+		gc.clear(Calendar.MINUTE);
+		gc.clear(Calendar.SECOND);
+		gc.clear(Calendar.MILLISECOND);
+		result.setFechaInicio(gc.getTime());
+		
+		gc.add(Calendar.MONTH, 1);
+		gc.add(Calendar.DAY_OF_MONTH, -1);
+		result.setFechaFinal(gc.getTime());
+
 		result.setId(new Long(getListaResumen().size() + 1));
 		return result;
 	}
--- reportes-rest/src/main/java/co/rcbike/reportes/model/fabrica/ReporteSemanal.java
+++ reportes-rest/src/main/java/co/rcbike/reportes/model/fabrica/ReporteSemanal.java
@@ -1,6 +1,8 @@
 package co.rcbike.reportes.model.fabrica;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import co.rcbike.reportes.model.ResumenWeb;
@@ -10,8 +12,24 @@ public class ReporteSemanal extends ReporteBasico implements IReporteRecorridos 
 
 	@Override
 	protected ResumenWeb crearResumen(Date fecha) {
-		// TODO Auto-generated method stub
-		return null;
+		ResumenWeb result = new ResumenWeb();
+
+		GregorianCalendar gc = new GregorianCalendar();
+		gc.setTime(fecha);
+		gc.set(Calendar.HOUR_OF_DAY, 0); // ! clear would not reset the hour of day !
+		gc.clear(Calendar.MINUTE);
+		gc.clear(Calendar.SECOND);
+		gc.clear(Calendar.MILLISECOND);
+		
+		gc.set(Calendar.DAY_OF_WEEK, gc.getFirstDayOfWeek());
+		result.setFechaInicio(gc.getTime());
+		
+		gc.add(Calendar.WEEK_OF_YEAR, 1);
+		gc.add(Calendar.DAY_OF_MONTH, -1);
+		result.setFechaFinal(gc.getTime());
+		
+		result.setId(new Long(getListaResumen().size() + 1));
+		return result;
 	}
 
 }
