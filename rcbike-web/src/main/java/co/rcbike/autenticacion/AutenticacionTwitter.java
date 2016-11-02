b9d164f41cc47e367ab19148d9ab9ff8f639e102
Felipe <notclevermail@gmail.com>
2016-11-02 01:01:26 -0500

    fix autenticacion twitter

--- rcbike-web/src/main/java/co/rcbike/autenticacion/AutenticacionTwitter.java
+++ rcbike-web/src/main/java/co/rcbike/autenticacion/AutenticacionTwitter.java
@@ -22,7 +22,7 @@ public class AutenticacionTwitter implements Serializable {
     @PostConstruct
     public void init() {
         twitter = TwitterFactory.getSingleton();
-        twitter.setOAuthConsumer("pzJH9IW2nAcFLWFB6iw9mtxI2", " zFN0CMS8gPerfifG2VxwEV168LshKv4yzFZgLPEjn5O7Dj7fMy");
+        twitter.setOAuthConsumer("pzJH9IW2nAcFLWFB6iw9mtxI2", "zFN0CMS8gPerfifG2VxwEV168LshKv4yzFZgLPEjn5O7Dj7fMy");
         log.debug("Inicializado " + this.getClass().getName());
     }
 
@@ -30,4 +30,42 @@ public class AutenticacionTwitter implements Serializable {
         RequestToken requestToken = twitter.getOAuthRequestToken("http://localhost:8080/rcbike-web/twitter");
         log.info(requestToken);
     }
+    /*
+     * public void requestToken() throws Exception { Client cl =
+     * ClientBuilder.newClient();
+     * 
+     * WebTarget mainEndpoint =
+     * cl.target("https://api.twitter.com/oauth/request_token");
+     * mainEndpoint.queryParam("oauth_callback",
+     * "http://localhost:8080/rcbike-web/twitter");
+     * mainEndpoint.queryParam("oauth_consumer_key",
+     * "pzJH9IW2nAcFLWFB6iw9mtxI2");
+     * mainEndpoint.queryParam("oauth_signature_method", "HMAC-SHA1");
+     * mainEndpoint.queryParam("oauth_signature", "" +
+     * calculateRFC2104HMAC("pzJH9IW2nAcFLWFB6iw9mtxI2",
+     * "zFN0CMS8gPerfifG2VxwEV168LshKv4yzFZgLPEjn5O7Dj7fMy"));
+     * mainEndpoint.queryParam("oauth_nonce", System.currentTimeMillis() +
+     * 8795); mainEndpoint.queryParam("oauth_timestamp", "" +
+     * System.currentTimeMillis() / 1000);
+     * mainEndpoint.queryParam("oauth_version", "1.0");
+     * 
+     * Map post2 = mainEndpoint.request().post(null, Map.class);
+     * 
+     * log.info(post2); // log.info(Status.fromStatusCode(post.getStatus())); }
+     * 
+     * private static final String HMAC_SHA1_ALGORITHM = "HmacSHA1";
+     * 
+     * private static String toHexString(byte[] bytes) { Formatter formatter =
+     * new Formatter();
+     * 
+     * for (byte b : bytes) { formatter.format("%02x", b); }
+     * 
+     * return formatter.toString(); }
+     * 
+     * public static String calculateRFC2104HMAC(String data, String key) throws
+     * SignatureException, NoSuchAlgorithmException, InvalidKeyException {
+     * SecretKeySpec signingKey = new SecretKeySpec(key.getBytes(),
+     * HMAC_SHA1_ALGORITHM); Mac mac = Mac.getInstance(HMAC_SHA1_ALGORITHM);
+     * mac.init(signingKey); return toHexString(mac.doFinal(data.getBytes())); }
+     */
 }
