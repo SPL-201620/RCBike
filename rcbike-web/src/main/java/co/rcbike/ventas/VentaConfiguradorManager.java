6ed41371c0d9d461be3970cecdfc60b67871ec33
juanc <juanc@juanc>
2016-11-22 23:04:37 -0500

    Mejoras configuracion y venta de bicicletas

--- rcbike-web/src/main/java/co/rcbike/ventas/VentaConfiguradorManager.java
+++ rcbike-web/src/main/java/co/rcbike/ventas/VentaConfiguradorManager.java
@@ -9,33 +9,35 @@ import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 import javax.inject.Inject;
 
+import lombok.Getter;
+import lombok.Setter;
 import co.rcbike.autenticacion.AutenticacionManager;
 import co.rcbike.configurador_bici.ConfiguradorGateway;
 import co.rcbike.configurador_bici.model.ConfiguracionWeb;
 import co.rcbike.gui.ModulosManager;
-import lombok.Getter;
-import lombok.Setter;
 
 @SuppressWarnings("serial")
 @ManagedBean
 @ViewScoped
 public class VentaConfiguradorManager implements Serializable {
 
-    @Getter
-    @Setter
-    private List<ConfiguracionWeb> listConfiguraciones;
+	@Getter
+	@Setter
+	private List<ConfiguracionWeb> listConfiguraciones;
 
-    @Inject
-    private ConfiguradorGateway gateway;
+	@Inject
+	private ConfiguradorGateway gateway;
 
-    @Getter
-    @Setter
-    @ManagedProperty(value = "#{modulosManager}")
-    private ModulosManager modulosManager;
+	@Getter
+	@Setter
+	@ManagedProperty(value = "#{modulosManager}")
+	private ModulosManager modulosManager;
 
-    @PostConstruct
-    public void init() {
-        gateway.listConfiguracionesByEmail(AutenticacionManager.emailAutenticado());
-    }
+	@PostConstruct
+	public void init() {
+		listConfiguraciones = gateway
+				.listConfiguracionesByEmail(AutenticacionManager
+						.emailAutenticado());
+	}
 
 }
