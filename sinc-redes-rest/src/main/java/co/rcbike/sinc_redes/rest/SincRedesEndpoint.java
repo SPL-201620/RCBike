90663248489d71d7024e0b19a63a75135f2812b3
Armando Perea Mora <a.perea10@uniandes.edu.co>
2016-11-23 17:32:37 -0500

    Implementación de publicación en Facebook y Twitter

--- sinc-redes-rest/src/main/java/co/rcbike/sinc_redes/rest/SincRedesEndpoint.java
+++ sinc-redes-rest/src/main/java/co/rcbike/sinc_redes/rest/SincRedesEndpoint.java
@@ -2,15 +2,79 @@ package co.rcbike.sinc_redes.rest;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 
+import co.rcbike.sinc_redes.model.OperacionesSincronizacion;
 import co.rcbike.sinc_redes.service.SincRedesService;
+import twitter4j.TwitterResponse;
 
-@Path("/sincronizacion")
+@Path(OperacionesSincronizacion.EP_SINCRONIZACION)
 @RequestScoped
 public class SincRedesEndpoint {
 
-    @Inject
-    private SincRedesService service;
+	@Inject
+	private SincRedesService service;
+
+	@GET
+	@Path("/" + OperacionesSincronizacion.ALIVE)
+	@Produces(MediaType.APPLICATION_JSON)
+	public String alive() {
+		return "endpoint alive";
+	}
+
+	/**
+	 * REST:
+	 * GET,/sincronizacion/publicarEnFacebook/userId=userId,accessToken=accessToken,message=message
+	 * 
+	 * Crea una entrada en el muro de Facebook
+	 * 
+	 * @param userId
+	 *            Identificador de usuario en Facebook
+	 * @param accessToken
+	 *            Identificador del token con permisos de Facebook
+	 * @param message
+	 *            Mensaje para colocar en el feed
+	 */
+	@POST
+	@Path("/" + OperacionesSincronizacion.OP_PUBLICAR_EN_FACEBOOK)
+	@Produces(MediaType.APPLICATION_JSON)
+	public String publicarEnFacebook(@QueryParam(OperacionesSincronizacion.PARAM_USER_ID) String userId,
+			@QueryParam(OperacionesSincronizacion.PARAM_ACCESS_TOKEN) String accessToken,
+			@QueryParam(OperacionesSincronizacion.PARAM_MESSAGE) String message) {
+		return service.postOnFacebook(userId, accessToken, message);
+	}
+
+	/**
+	 * REST:
+	 * GET,/sincronizacion/publicarEnTwitter/userId=userId,accessToken=accessToken,message=message
+	 * 
+	 * Crea una entrada en el muro de Facebook
+	 * 
+	 * @param consumerKey
+	 *            consumerKeyStr
+	 * @param consumerSecret
+	 *            consumerSecretStr
+	 * @param accessToken
+	 *            accessTokenStr
+	 * @param accessTokenSecret
+	 *            accessTokenSecretStr
+	 * @param message
+	 *            Mensaje para colocar en el feed
+	 */
+	@POST
+	@Path("/" + OperacionesSincronizacion.OP_PUBLICAR_EN_TWITTER)
+	@Produces(MediaType.APPLICATION_JSON)
+	public TwitterResponse publicarEnTwitter(@QueryParam(OperacionesSincronizacion.PARAM_CONSUMER_KEY_STR) String consumerKey,
+			@QueryParam(OperacionesSincronizacion.PARAM_CONSUMER_SECRET_STR) String consumerSecret,
+			@QueryParam(OperacionesSincronizacion.PARAM_ACCESS_TOKEN_STR) String accessToken,
+			@QueryParam(OperacionesSincronizacion.PARAM_ACCESS_TOKEN_SECRET_STR) String accessTokenSecret,
+			@QueryParam(OperacionesSincronizacion.PARAM_MESSAGE) String message) {
+		return service.postOnTwitter(consumerKey, consumerSecret, accessToken, accessTokenSecret, message);
+	}
 
 }
--- sinc-redes-rest/src/main/java/co/rcbike/sinc_redes/service/SincRedesService.java
+++ sinc-redes-rest/src/main/java/co/rcbike/sinc_redes/service/SincRedesService.java
@@ -1,19 +1,49 @@
 
 package co.rcbike.sinc_redes.service;
 
-import java.util.logging.Logger;
-
 import javax.ejb.Stateless;
-import javax.inject.Inject;
-import javax.persistence.EntityManager;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.TwitterResponse;
+import twitter4j.auth.AccessToken;
 
 @Stateless
 public class SincRedesService {
 
-    @Inject
-    private Logger log;
+	public String postOnFacebook(String userId, String accessToken, String message) {
+		Client cl = ClientBuilder.newClient();
+		WebTarget mainEndpoint = cl.target("https://graph.facebook.com/v2.8").path(userId).path("feed");
+		WebTarget webTarget = mainEndpoint.queryParam("access_token", accessToken).queryParam("message", message);
 
-    @Inject
-    private EntityManager em;
+		Response postResponse = webTarget.request(MediaType.APPLICATION_JSON_TYPE)
+				.post(Entity.entity("A string entity to be POSTed", MediaType.TEXT_PLAIN));
+		return postResponse.readEntity(String.class);
+	}
+
+	public TwitterResponse postOnTwitter(String consumerKey, String consumerSecret, String accessToken,
+			String accessTokenSecret, String message) {
+		Twitter twitter = new TwitterFactory().getInstance();
+
+		twitter.setOAuthConsumer(consumerKey, consumerSecret);
+		AccessToken token = new AccessToken(accessToken, accessTokenSecret);
+
+		twitter.setOAuthAccessToken(token);
+
+		try {
+			Status status = twitter.updateStatus(message);
+			return status;
+		} catch (TwitterException e) {
+			return e;
+		}
+	}
 
 }
